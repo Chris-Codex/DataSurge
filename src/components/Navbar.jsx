457df7f90087ceaf721375ded087a3fb9c58f3b4
Admin <admin@Admins-MacBook-Pro.local>
2022-12-25 06:23:11 +0000

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { AiOutlineClose } from "react-icons/ai";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
   return (
     <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
       <h1 className="w-full font-bold text-3xl text-[#09A837] tracking-normal leading-none">
         DataSurge
       </h1>
-      <ul className="flex hidden">
+      <ul className="hidden md:flex">
         <li className="p-4">Home</li>
         <li className="p-4">About</li>
         <li className="p-4">Resources</li>
         <li className="p-4">Company</li>
         <li className="p-4">Contact</li>
       </ul>
-      <div>
-        <GiHamburgerMenu />
+
+      <div onClick={handleNav} className="block md:hidden">
+        {!nav ? <AiOutlineClose size={20} /> : <GiHamburgerMenu size={20} />}
       </div>
-      <div className="fixed top-0 left-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300]">
+      <div
+        className={
+          !nav
+            ? "fixed top-0 left-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500"
+            : "fixed left-[100%]"
+        }
+      >
         <h1 className="w-full font-bold text-3xl m-4 text-[#09A837] tracking-normal leading-none">
           DataSurge
         </h1>
